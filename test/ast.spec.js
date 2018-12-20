2e54a858fac056347e894c00db96dbc4f12c27aa
Tyler Long <tyler4long@gmail.com>
2018-12-20 15:52:47 +0800

    Add more test cases

--- test/ast.spec.js
+++ test/ast.spec.js
@@ -1,9 +1,24 @@
 /* eslint-env jest */
-import process from '../src/ast'
+import parse from '../src/ast'
 
 describe('AST', () => {
+  test('Single node', () => {
+    const ast = parse(`graph TD
+A`)
+    expect(ast).toEqual({
+      'direction': 'TD',
+      'expressions': [
+        {
+          'node1': {
+            'id': 'A'
+          }
+        }
+      ]
+    })
+  })
+
   test('A --> B', () => {
-    const ast = process(`graph LR
+    const ast = parse(`graph LR
 A --> B`)
     expect(ast).toEqual({
       'direction': 'LR',
@@ -20,15 +35,36 @@ A --> B`)
       ]
     })
   })
-  test('Single node', () => {
-    const ast = process(`graph TD
-A`)
+
+  test('iii --- jjj', () => {
+    const ast = parse(`graph LR
+iii --- jjj`)
+    expect(ast).toEqual({
+      'direction': 'LR',
+      'expressions': [
+        {
+          'node1': {
+            'id': 'iii'
+          },
+          'edge': '---',
+          'node2': {
+            'id': 'jjj'
+          }
+        }
+      ]
+    })
+  })
+
+  test('A[hello world]', () => {
+    const ast = parse(`graph TD
+A[hello world]`)
     expect(ast).toEqual({
       'direction': 'TD',
       'expressions': [
         {
           'node1': {
-            'id': 'A'
+            'id': 'A',
+            'data': '[hello world]'
           }
         }
       ]
