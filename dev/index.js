ed6c9a40c7e30b33d030d70185bb316745f88bee
Tyler Long <tyler4long@gmail.com>
2018-12-22 12:13:32 +0800

    Simplify code

--- dev/index.js
+++ dev/index.js
@@ -7,18 +7,17 @@ import { toDagreGraph } from '../src'
 // turn <pre> to <svg>
 const renderElement = () => {
   document.querySelectorAll('.flowchart').forEach(element => {
-    // replace element with <svg width=512 height=384><g></g></svg>
+    // replace element with <svg width=512 height=384></svg>
     const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
     svg.setAttribute('width', 512)
     svg.setAttribute('height', 384)
-    svg.innerHTML = '<g></g>'
     element.parentNode.replaceChild(svg, element)
 
     // render graph to <svg>
     const Render = dagreD3.render
     const render = new Render()
     const dagreGraph = toDagreGraph(element.innerText)
-    render(d3.select(svg).select('g'), dagreGraph)
+    render(d3.select(svg), dagreGraph)
   })
 }
 
