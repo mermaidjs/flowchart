978a0e877b1e13c7211cf184fa3843b2247cbee3
Tyler Long <tyler4long@gmail.com>
2018-12-22 12:26:48 +0800

    Extract method renderElements

--- dev/index.js
+++ dev/index.js
@@ -1,24 +1,4 @@
-import dagreD3 from 'dagre-d3-renderer'
-import * as d3 from 'd3'
-
 import '../src/themes/forest.css'
-import { toDagreGraph } from '../src'
+import { renderElements } from '../src'
 
-// turn <pre> to <svg>
-const renderElement = () => {
-  document.querySelectorAll('.flowchart').forEach(element => {
-    // replace element with <svg width=512 height=384></svg>
-    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-    svg.setAttribute('width', 512)
-    svg.setAttribute('height', 384)
-    element.parentNode.replaceChild(svg, element)
-
-    // render graph to <svg>
-    const Render = dagreD3.render
-    const render = new Render()
-    const dagreGraph = toDagreGraph(element.innerText)
-    render(d3.select(svg), dagreGraph)
-  })
-}
-
-renderElement()
+renderElements('.flowchart')
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import graphlib from 'graphlibrary'
 import * as R from 'ramda'
+import dagreD3 from 'dagre-d3-renderer'
+import * as d3 from 'd3'
 
 import { toAst } from './ast'
 
@@ -32,4 +34,20 @@ export const toDagreGraph = input => {
   })
 
   return graph
+}
+
+export const renderElements = selectors => {
+  document.querySelectorAll(selectors).forEach(element => {
+    // replace element with <svg>
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+    svg.setAttribute('width', 512)
+    svg.setAttribute('height', 384)
+    element.parentNode.replaceChild(svg, element)
+
+    // render graph to <svg>
+    const Render = dagreD3.render
+    const render = new Render()
+    const dagreGraph = toDagreGraph(element.innerText)
+    render(d3.select(svg), dagreGraph)
+  })
 }
