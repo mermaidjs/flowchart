14a935b24de6ac8a68911ebe2fdf899dccbc769b
Tyler Long <tyler4long@gmail.com>
2018-12-22 11:52:55 +0800

    Render multiple graphs by selector in the current page

--- dev/index.js
+++ dev/index.js
@@ -6,17 +6,18 @@ import { toDagreGraph } from '../src'
 
 // turn <pre> to <svg>
 const renderElement = () => {
-  d3.select('.flowchart').each(function () {
-    const dagreGraph = toDagreGraph(this.innerText)
-
+  document.querySelectorAll('.flowchart').forEach(element => {
+    // replace element with <svg width=512 height=384><g></g></svg>
     const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
     svg.setAttribute('width', 512)
     svg.setAttribute('height', 384)
     svg.innerHTML = '<g></g>'
-    this.parentNode.replaceChild(svg, this)
+    element.parentNode.replaceChild(svg, element)
 
+    // render graph to <svg>
     const Render = dagreD3.render
     const render = new Render()
+    const dagreGraph = toDagreGraph(element.innerText)
     render(d3.select(svg).select('g'), dagreGraph)
   })
 }
